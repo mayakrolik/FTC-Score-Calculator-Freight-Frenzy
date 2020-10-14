837f527e68013f48bdb9cb5c2340e04ae7accaae
mayakrolik <maya.ak8848@gmail.com>
2020-10-13 22:58:46 -0700

    navigation bar layout complete

---
+++ app/src/main/java/com/example/ftcscorecalculatorbeta/ui/progress/ProgressViewModel.java
@@ -0,0 +1,19 @@
+package com.example.ftcscorecalculatorbeta.ui.progress;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class ProgressViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public ProgressViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is home fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
