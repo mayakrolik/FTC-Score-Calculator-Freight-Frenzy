b34697de562c85a8de79f10fca5f00e08eb831c8
Krolik, Martin <mkrolik@ucsd.edu>
2020-11-01 18:18:54 -0800

    Navigation bar works now

--- app/src/main/java/com/example/ftcscorecalculatorbeta/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/ftcscorecalculatorbeta/ui/home/HomeFragment.java
@@ -5,17 +5,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
 
 import com.example.ftcscorecalculatorbeta.R;
 
@@ -38,14 +32,4 @@ public class HomeFragment extends Fragment {
         return root;
     }
 
-    @Override
-    public void onViewCreated(@NonNull View view,
-                              @Nullable Bundle savedInstanceState) {
-        NavController navController = Navigation.findNavController(view);
-        AppBarConfiguration appBarConfiguration =
-                new AppBarConfiguration.Builder(navController.getGraph()).build();
-        Toolbar toolbar = view.findViewById(R.id.nav_view);
-        NavigationUI.setupWithNavController(
-                toolbar, navController, appBarConfiguration);
-    }
 }
--- app/src/main/java/com/example/ftcscorecalculatorbeta/ui/progress/ProgressFragment.java
+++ app/src/main/java/com/example/ftcscorecalculatorbeta/ui/progress/ProgressFragment.java
@@ -1,21 +1,16 @@
 package com.example.ftcscorecalculatorbeta.ui.progress;
 
 import android.os.Bundle;
-        import android.view.LayoutInflater;
-        import android.view.View;
-        import android.view.ViewGroup;
-        import android.widget.TextView;
-
-        import androidx.annotation.NonNull;
-        import androidx.annotation.Nullable;
-import androidx.appcompat.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-        import androidx.lifecycle.Observer;
-        import androidx.lifecycle.ViewModelProviders;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
 
 import com.example.ftcscorecalculatorbeta.R;
 
@@ -25,8 +20,7 @@ public class ProgressFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        progressViewModel =
-                ViewModelProviders.of(this).get(ProgressViewModel.class);
+        progressViewModel =  ViewModelProviders.of(this).get(ProgressViewModel.class);
         View root = inflater.inflate(R.layout.fragment_progress, container, false);
         final TextView textView = root.findViewById(R.id.text_progress);
         progressViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
@@ -38,14 +32,4 @@ public class ProgressFragment extends Fragment {
         return root;
     }
 
-    @Override
-    public void onViewCreated(@NonNull View view,
-                              @Nullable Bundle savedInstanceState) {
-        NavController navController = Navigation.findNavController(view);
-        AppBarConfiguration appBarConfiguration =
-                new AppBarConfiguration.Builder(navController.getGraph()).build();
-        Toolbar toolbar = view.findViewById(R.id.nav_view);
-        NavigationUI.setupWithNavController(
-                toolbar, navController, appBarConfiguration);
-    }
 }
