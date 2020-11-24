ff4bfb515a7a5978b128f900ac51b984434284ae
mayakrolik <maya.ak8848@gmail.com>
2020-11-23 17:04:25 -0800

    Kudos working

--- app/src/main/java/com/example/ftcscorecalculatorbeta/data/model/Kudo.java
+++ app/src/main/java/com/example/ftcscorecalculatorbeta/data/model/Kudo.java
@@ -1,10 +1,12 @@
 package com.example.ftcscorecalculatorbeta.data.model;
 
+import com.google.firebase.Timestamp;
+
 public class Kudo {
     public String UserDisplayName;
     public String UserEmailAddress;
     public String UserUid;
-
+    public Timestamp CreatedTimestamp;
     public int TeamNumber;
     public String TeamNickName;
     /*
