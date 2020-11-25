b81ab4004884c6a35f99ca2424ecde60b628afd0
mayakrolik <maya.ak8848@gmail.com>
2020-11-25 14:17:29 -0800

    Team fragment updates with team member information

---
+++ app/src/main/java/com/example/ftcscorecalculatorbeta/RVTeamAdapter.java
@@ -0,0 +1,101 @@
+package com.example.ftcscorecalculatorbeta;
+
+import android.os.Bundle;
+import android.transition.AutoTransition;
+import android.transition.TransitionManager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CompoundButton;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TableLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.ftcscorecalculatorbeta.data.model.Kudo;
+import com.example.ftcscorecalculatorbeta.data.model.UserProfile;
+import com.example.ftcscorecalculatorbeta.data.model.Team;
+import com.example.ftcscorecalculatorbeta.ui.calculator.CalculatorViewModel;
+import com.example.ftcscorecalculatorbeta.ui.home.HomeViewModel;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.Timestamp;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FieldValue;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.auth.User;
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.messaging.RemoteMessage;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+import android.util.Log;
+
+public class RVTeamAdapter extends RecyclerView.Adapter<RVTeamAdapter.UserViewHolder> {
+
+    List<UserProfile> users;
+
+    public RVTeamAdapter(List<UserProfile> users) {
+        this.users = users;
+    }
+
+    final static String TAG = "RVTeamAdapter";
+
+    @NonNull
+    @Override
+    public RVTeamAdapter.UserViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.team_member, parent, false);
+        RVTeamAdapter.UserViewHolder holder = new RVTeamAdapter.UserViewHolder(view);
+        Log.d(TAG, "onCreateViewHolder");
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull UserViewHolder holder, int position) {
+
+        holder.UserInitials.setText(String.valueOf(users.get(position).DisplayName));
+        holder.UserEmail.setText(String.valueOf(users.get(position).EmailAddress));
+        holder.LoginCount.setText(String.valueOf(users.get(position).LoginCount));
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        Log.d(TAG, "getItemCount " + String.valueOf(users.size()));
+        return users.size();
+
+    }
+
+    @Override
+    public void onAttachedToRecyclerView(RecyclerView recyclerView) {
+        super.onAttachedToRecyclerView(recyclerView);
+    }
+
+    public static class UserViewHolder extends RecyclerView.ViewHolder {
+        TextView UserInitials;
+        TextView UserEmail;
+        TextView LoginCount;
+
+        UserViewHolder(final View itemView) {
+            super(itemView);
+            UserInitials = (TextView) itemView.findViewById(R.id.member_name);
+            UserEmail = (TextView) itemView.findViewById(R.id.user_email);
+            LoginCount = (TextView) itemView.findViewById(R.id.login_count);
+
+        }
+    }
+
+}
--- app/src/main/java/com/example/ftcscorecalculatorbeta/ui/team/TeamFragment.java
+++ app/src/main/java/com/example/ftcscorecalculatorbeta/ui/team/TeamFragment.java
@@ -1,33 +1,120 @@
 package com.example.ftcscorecalculatorbeta.ui.team;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.ftcscorecalculatorbeta.MainActivity;
 import com.example.ftcscorecalculatorbeta.R;
+import com.example.ftcscorecalculatorbeta.RVScoreAdapter;
+import com.example.ftcscorecalculatorbeta.RVTeamAdapter;
+import com.example.ftcscorecalculatorbeta.data.model.Score;
+import com.example.ftcscorecalculatorbeta.data.model.UserProfile;
 import com.example.ftcscorecalculatorbeta.ui.calculator.CalculatorViewModel;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.firestore.auth.User;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class TeamFragment extends Fragment {
 
+    private boolean blnInitalized = false;
     private TeamViewModel teamViewModel;
+    private TextView TeamName;
+    private TextView TeamNumber;
+    private TextView State;
+    private TextView City;
+    private TextView RookieYear;
+    private TextView Level;
 
     final static String TAG = "TeamFragment";
-    public void onCreate(Bundle savedInstanceState) {
+
+    /*public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+    }*/
+
+    private void queryForUsersForMyTeam() {
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        MainActivity activity = (MainActivity) getActivity();
+
+        db.collection("Users")
+                .whereEqualTo("TeamNumber", activity.getMyTeam().TeamNumber)
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d(TAG, document.getId() + " => " + document.getData());
+                                UserProfile userProfile = document.toObject(UserProfile.class);
+                                users.add(userProfile);
+                            }
+
+                            RecyclerView rv = (RecyclerView) getView().findViewById(R.id.rv_team);
+                            RVTeamAdapter adapter = new RVTeamAdapter(users);
+                            rv.setAdapter(adapter);
+                        } else {
+                            Log.d(TAG, "Error getting documents: ", task.getException());
+                        }
+                    }
+                });
+
+
     }
+
+    private List<UserProfile> users = new ArrayList<>();
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         teamViewModel =
                 ViewModelProviders.of(this).get(TeamViewModel.class);
         View root = inflater.inflate(R.layout.fragment_team, container, false);
-        //doOnCreate(root);
+        doOnCreate(root);
+        queryForUsersForMyTeam();
+        RecyclerView rv_team = (RecyclerView) root.findViewById(R.id.rv_team);
+        LinearLayoutManager llm = new LinearLayoutManager(getContext());
+        rv_team.setLayoutManager(llm);
+
+        RVTeamAdapter adapter = new RVTeamAdapter(users);
+        rv_team.setAdapter(adapter);
+
         return root;
+    }
+
+
+    private void doOnCreate(View view)
+    {
+        TeamName = view.findViewById(R.id.team_team_name);
+        TeamNumber = view.findViewById(R.id.team_team_number);
+        State = view.findViewById(R.id.team_team_state);
+        City = view.findViewById(R.id.team_team_city);
+        RookieYear = view.findViewById(R.id.rookie_year);
+        Level = view.findViewById(R.id.level);
+
+
+        MainActivity activity = (MainActivity) getActivity();
+        TeamName.setText(activity.getMyTeam().NickName);
+        TeamNumber.setText("#" + activity.getMyTeam().TeamNumber);
+        State.setText(activity.getMyTeam().StateProv);
+        City.setText(activity.getMyTeam().City);
+        RookieYear.setText(String.valueOf(activity.getMyTeam().RookieYear));
+        Level.setText(activity.getMyTeam().ProgramName);
+
     }
 
 }
